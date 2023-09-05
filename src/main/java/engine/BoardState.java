ffd195485d5e146d232d5d7c9940f8e9678c4c3f
K-San-official <63699796+K-San-official@users.noreply.github.com>
2023-09-05 13:32:25 +0200

    added structure

--- src/main/java/engine/BoardState.java
+++ src/main/java/engine/BoardState.java
@@ -1,4 +1,32 @@
 package engine;
 
+import java.util.List;
+
 public class BoardState {
+    private int[][] field; // First dimension: Numbers representing A-J
+
+    public BoardState() {
+        field = new int[10][10];
+    }
+
+    private boolean isAdjacent(int x1, int y1, int x2, int y2) {
+        //TODO: Implement
+        return false;
+    }
+
+    private boolean didPlayerWin(int player) {
+        //TODO: Implement
+        return false;
+    }
+
+
+    //TODO: Rethink the whole structure of how to check if a piece can be taken and integrate this into the move.
+    private boolean canTake(int player) {
+        //TODO: Implement
+        return false;
+    }
+
+    private int[][] piecesToTake(int player) {
+        return null;
+    }
 }
